fa3fdd3d3f7f5461051c79b68d73cc12a5825f21
testelalalalallalalala <122115093@ulife.com.br>
2025-10-07 20:08:01 -0300

    Add files via upload

---
+++ ex7.java
@@ -0,0 +1,13 @@
+//Faça um programa que imprima na tela os valores no intervalo entre 10 e 200.
+
+package aula1;
+public class ex7 {
+    public static void main(String[] args) {
+        System.out.println("Números de 10 até 200:");
+        
+        for (int i = 10; i <= 200; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
